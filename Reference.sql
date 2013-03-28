d8764f28c089b1c76f9d837438363f7065c95884
RiskManagement <profilesarehardtopick@gmail.com>
2013-03-28 14:26:12 -0500

    Update Reference.sql

--- Reference.sql
+++ Reference.sql
@@ -71,3 +71,75 @@ FROM @report t1
 	INNER JOIN ConnectionStrings c ON t1.TableID = c.ConnectionID
 
 END
+
+/*
+
+Another alternative way to establish connection strings using individual server, database, schema and table names:
+
+CREATE TABLE ConnectionStrings(
+	ConnectionID INT IDENTITY(1,1) PRIMARY KEY,
+	ServerName VARCHAR(100),
+	DatabaseName VARCHAR(100),
+	SchemaName VARCHAR(100),
+	TableName VARCHAR(100),
+	ConnectionString VARCHAR(500)
+)
+
+INSERT INTO ConnectionStrings (ServerName,DatabaseName,SchemaName,TableName)
+VALUES('[SERVERONE]','[dbone]','[dbo]','[TableOne]')
+	,('[SERVERTWO]','[dbtwo]','[dob]','[TableTwo]')
+	,('[SERVERTHREE]','[dbthree]','[bdo]','[TableThree]')
+
+UPDATE ConnectionStrings
+SET ConnectionString = ServerName + '.' + DatabaseName + '.' + SchemaName + '.' + TableName
+
+CREATE PROCEDURE sp_CheckTableCounts
+AS
+BEGIN
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+DECLARE @start INT
+SET @start = 1
+DECLARE @total INT 
+SELECT @total = MAX(ConnectionID) FROM ConnectionStrings
+SET @total = @total + 1
+DECLARE @sql NVARCHAR(MAX)
+
+WHILE @start < @total
+BEGIN
+	DECLARE @conn NVARCHAR(500)
+	SELECT @conn = ConnectionString FROM ConnectionStrings WHERE ConnectionID = @start
+	
+	SET @sql = 'INSERT INTO TableRowCount (TableID, [Row Count])
+	SELECT ' + CAST(@start AS NVARCHAR(20)) +', COUNT(*)
+	FROM ' + @conn
+
+	EXECUTE(@sql)
+	
+	SET @start = @start + 1
+END
+
+-- Reporting on the row count change:
+DECLARE @report TABLE(
+	TableID INT,
+	[Row Count] DECIMAL(20,2),
+	Timestamp SMALLDATETIME
+)
+
+-- Get the last two minutes of row counts
+INSERT INTO @report
+SELECT *
+FROM TableRowCount
+WHERE Timestamp BETWEEN DATEADD(MI,-2,GETDATE()) AND GETDATE()
+
+SELECT c.ConnectionString AS "Server and Table Name"
+	,((t2.[Row Count]-t1.[Row Count])/(NULLIF(t1.[Row Count],0)))*100 AS "Delta"
+	,t1.[Row Count], t2.[Row Count], t1.Timestamp, t2.Timestamp
+FROM @report t1
+	INNER JOIN @report t2 ON t1.TableID = t2.TableID AND t2.Timestamp = DATEADD(MI,+1,t1.Timestamp)
+	INNER JOIN ConnectionStrings c ON t1.TableID = c.ConnectionID
+
+END
+
+*/
